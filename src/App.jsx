0aff24b57c231143aacf7b22750eec970bd3c09a
amaykorade <amaykorade5@gmail.com>
2025-03-26 00:30:06 +0530

    Complete frontend

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       {/* <ProductCard /> */}
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/product" element={<Product />} />
           <Route path="/product/:productId" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
